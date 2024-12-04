ce62474f0e015ce7b3270e03e9407c7d9c6abd7c
Jonathan Franzi <jonoerik@fastmail.fm>
2024-12-04 12:24:18 +1100

    Complete day03 part1

---
+++ day03/day03.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import re
+
+InputType = str
+ResultType = int
+
+
+def load(input_path: Path) -> InputType:
+    with open(input_path) as f:
+        return f.read().strip()
+
+
+def part1(input_data: InputType) -> ResultType:
+    mul_regex = re.compile(r"mul\((?P<x>[0-9]{1,3}),(?P<y>[0-9]{1,3})\)")
+    return sum([int(re_match.group("x")) * int(re_match.group("y")) for re_match in mul_regex.finditer(input_data)])
+
+
+def part2(input_data: InputType) -> ResultType:
+    pass  # TODO
