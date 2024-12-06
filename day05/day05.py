866a94fbd60f13e3de4da346a19cf2698d9262c1
Jonathan Franzi <jonoerik@fastmail.fm>
2024-12-06 22:39:59 +1100

    Complete day05 part2

--- day05/day05.py
+++ day05/day05.py
@@ -29,22 +29,57 @@ def load(input_path: Path) -> InputType:
     return rules, updates
 
 
+def update_valid(rules: list[RuleType], update: UpdateType) -> bool:
+    for r in rules:
+        if r[0] in update and r[1] in update and update.index(r[1]) < update.index(r[0]):
+            return False
+    return True
+
+
 def part1(input_data: InputType) -> ResultType:
     rules, updates = input_data
     total = 0
 
-    def update_valid(u: UpdateType) -> bool:
-        for r in rules:
-            if r[0] in u and r[1] in u and u.index(r[1]) < u.index(r[0]):
-                return False
-        return True
-
     for update in updates:
-        if update_valid(update):
+        if update_valid(rules, update):
             total += update[len(update) // 2]
 
     return total
 
 
 def part2(input_data: InputType) -> ResultType:
-    pass
+    rules, updates = input_data
+    total = 0
+
+    def fix_update(u: list[int]) -> list[int]:
+        # The rules can be treated as edges in a directed graph, where a->b means that a must be sorted before b in
+        # valid updates.
+        # To fix an update, take the subset of rules a->b where both a and b appear in the update, construct the graph
+        # of those rules. A valid update will be a topological ordering of the nodes. As we expect a valid update to
+        # exist, a topological ordering will be possible (i.e. the rules subset graph will be a DAG).
+
+        # Use Kahn's Algorithm to find the topological ordering.
+        # Topological Sorting, Wikipedia, accessed 2024-12-06
+        # https://en.wikipedia.org/w/index.php?title=Topological_sorting&oldid=1258030734#Kahn's_algorithm
+        graph = [(a, b) for a, b in rules if a in u and b in u]
+        source_nodes = {a for a,b in graph}
+        destination_nodes = {b for a,b in graph}
+        L = []
+        S = source_nodes - destination_nodes
+        while S:
+            n = S.pop()
+            L.append(n)
+            for m in {x for x in destination_nodes if (n, x) in graph}:
+                graph.remove((n, m))
+                if m not in [b for a, b in graph]:
+                    S.add(m)
+
+        assert not graph
+        return L
+
+    for update in updates:
+        if not update_valid(rules, update):
+            update = fix_update(update)
+            total += update[len(update) // 2]
+
+    return total
