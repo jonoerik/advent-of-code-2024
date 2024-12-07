01e37ac05c8bcbb88f54d4c121aae98f08ed39e6
Jonathan Franzi <jonoerik@fastmail.fm>
2024-12-07 22:15:31 +1100

    Complete day06 part2

--- day06/day06.py
+++ day06/day06.py
@@ -32,8 +32,8 @@ def load(input_path: Path) -> InputType:
     return floor_map, guard
 
 
-def part1(input_data: InputType) -> ResultType:
-    visited_positions = set()
+def visited_positions(input_data: InputType) -> set[tuple[int, int]]:
+    result = set()
 
     floor_map, (guard_r, guard_c, guard_dir) = input_data
     dr, dc = {Direction.UP: (-1, 0),
@@ -43,7 +43,7 @@ def part1(input_data: InputType) -> ResultType:
               }[guard_dir]
 
     while 0 <= guard_r < len(floor_map) and 0 <= guard_c < len(floor_map[0]):
-        visited_positions.add((guard_r, guard_c))
+        result.add((guard_r, guard_c))
         if 0 <= guard_r + dr < len(floor_map) and 0 <= guard_c + dc < len(floor_map[0]) \
                 and floor_map[guard_r + dr][guard_c + dc]:
             # Turn 90 degrees to the right.
@@ -52,8 +52,41 @@ def part1(input_data: InputType) -> ResultType:
             guard_r += dr
             guard_c += dc
 
-    return len(visited_positions)
+    return result
+
+
+def part1(input_data: InputType) -> ResultType:
+    vp = visited_positions(input_data)
+    return len(vp)
 
 
 def part2(input_data: InputType) -> ResultType:
-    pass
+    floor_map, (guard_r, guard_c, guard_dir) = input_data
+    # Only try putting the obstacle in a position that the original path would collide with.
+    trial_obstacles = visited_positions(input_data) - {(guard_r, guard_c)}
+
+    def creates_loop(obs: tuple[int, int]) -> bool:
+        previous_positions = set()
+        r = guard_r
+        c = guard_c
+        dr, dc = {Direction.UP: (-1, 0),
+                  Direction.RIGHT: (0, 1),
+                  Direction.DOWN: (1, 0),
+                  Direction.LEFT: (0, -1)
+                  }[guard_dir]
+
+        while 0 <= r < len(floor_map) and 0 <= c < len(floor_map[0]):
+            if (r, c, dr ,dc) in previous_positions:
+                return True
+            previous_positions.add((r, c, dr, dc))
+            if 0 <= r + dr < len(floor_map) and 0 <= c + dc < len(floor_map[0]) \
+                    and (floor_map[r + dr][c + dc] or (r + dr, c + dc) == obs):
+                # Turn 90 degrees to the right.
+                dr, dc = dc, dr * -1
+            else:
+                r += dr
+                c += dc
+
+        return False
+
+    return len([obstacle for obstacle in trial_obstacles if creates_loop(obstacle)])
