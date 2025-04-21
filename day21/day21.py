adc7004cefb168678b854b7d04cbbccd9bd2e2c2
Jonathan Franzi <jonoerik@fastmail.fm>
2025-04-21 16:02:36 +1000

    Change day21 to a quicker approach.
    Break each code down into individual number-pad transitions that are calculated separately.

--- day21/day21.py
+++ day21/day21.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from enum import Enum
+import itertools
 from pathlib import Path
 
 InputType = list[str]
@@ -23,10 +24,9 @@ class DirKey(Enum):
     RIGHT = 3
     A = 4
 
-# For each robot, the current key the robot is positioned to actuate, and the string of keys pressed on the final
-# number pad. Direction pad robots are all combined into a sub-tuple, to allow handling of different counts of these
-# robots.
-SearchState = tuple[tuple[DirKey, ...], NumKey, str]
+# For each robot, the current key the robot is positioned to actuate. Direction pad robots are all combined into a
+# sub-tuple, to allow handling of different counts of these robots.
+SearchState = tuple[tuple[DirKey, ...], NumKey]
 
 
 def next_num_key(current_key: NumKey, dir_key: DirKey) -> NumKey | None:
@@ -83,36 +83,45 @@ def next_state(s: SearchState, k: DirKey) -> SearchState | None:
         result_dirpad_robots.append(result_dirpad_robot)
         if prev_key != DirKey.A:
             result_dirpad_robots += s[0][len(result_dirpad_robots):]
-            return tuple(result_dirpad_robots), s[1], s[2]
+            return tuple(result_dirpad_robots), s[1]
         prev_key = result_dirpad_robot
     else:
         assert len(result_dirpad_robots) == len(s[0])
         result_numpad_robot = next_num_key(s[1], prev_key)
         if result_numpad_robot is None:
             return None
-        if prev_key == DirKey.A:
-            return tuple(result_dirpad_robots), result_numpad_robot, s[2] + ("A" if result_numpad_robot == AKey else str(result_numpad_robot))
-        return tuple(result_dirpad_robots), result_numpad_robot, s[2]
+        return tuple(result_dirpad_robots), result_numpad_robot
 
 
-def calculate_heuristic(code: str, s: SearchState) -> int:
-    """Return a heuristic estimate of the number manual button presses to reach the required code from state s."""
+def calculate_heuristic(end_key: NumKey, s: SearchState) -> int:
+    """Return a heuristic estimate of the number of manual button presses to reach the required end_key on the num pad,
+    from SearchState"""
+    #TODO
     return 0
 
 
-def shortest_sequence(code: str, dir_pad_robot_count: int) -> int:
-    """Use A* Algorithm to find the length of the shortest sequence of manual button presses to input the
-    required code into the numeric keypad."""
+shortest_sequence_next_key_memo: dict[tuple[NumKey, NumKey, int], int] = {}
+
+
+def shortest_sequence_next_key(start_key: NumKey, end_key: NumKey, dir_pad_robot_count: int) -> int:
+    """Use A* algorithm to find the length of the shortest sequence of manual number presses to input, to move the
+    number pad robot from key start_key to key end_key (without actuating it). All other robots start on A, and will
+    end on A as well."""
+    global shortest_sequence_next_key_memo
+    if (start_key, end_key, dir_pad_robot_count) in shortest_sequence_next_key_memo:
+        return shortest_sequence_next_key_memo[(start_key, end_key, dir_pad_robot_count)]
+
+    start_state = ((DirKey.A,) * dir_pad_robot_count, start_key)
     # {state: (current cost, current cost + heuristic)}
-    start_state = ((DirKey.A,) * dir_pad_robot_count, AKey, "")
-    to_visit: dict[SearchState, tuple[int, int]] = {start_state: (0, calculate_heuristic(code, start_state))}
+    to_visit: dict[SearchState, tuple[int, int]] = {start_state: (0, calculate_heuristic(end_key, start_state))}
     visited: set[SearchState] = set()
 
     while to_visit:
         current_state = min(to_visit, key=lambda x: to_visit[x][1])
         cost, total_heuristic = to_visit[current_state]
         del to_visit[current_state]
-        if current_state[2] == code:
+        if current_state == ((DirKey.A,) * dir_pad_robot_count, end_key):
+            shortest_sequence_next_key_memo[(start_key, end_key, dir_pad_robot_count)] = cost
             return cost
         assert current_state not in visited
         visited.add(current_state)
@@ -121,13 +130,23 @@ def shortest_sequence(code: str, dir_pad_robot_count: int) -> int:
             next_to_visit = next_state(current_state, key)
             if next_to_visit is None or next_to_visit in visited:
                 continue
-            if not code.startswith(next_to_visit[2]):
-                continue
             if next_to_visit not in to_visit or to_visit[next_to_visit][0] > cost + 1:
-                to_visit[next_to_visit] = (cost + 1, cost + 1 + calculate_heuristic(code, next_to_visit))
+                to_visit[next_to_visit] = (cost + 1, cost + 1 + calculate_heuristic(end_key, next_to_visit))
 
     # Should have found a suitable sequence before exhausting the search space.
     assert False
+
+
+def shortest_sequence(code: str, dir_pad_robot_count: int) -> int:
+    """Find the length of the shortest sequence of manual button presses to input the required code into the numeric
+    keypad."""
+    # Add up the cost of transitioning to each next numpad key, and the cost of pressing each key.
+    return sum(
+        [shortest_sequence_next_key(AKey if start_key == "A" else int(start_key),
+                                    AKey if end_key == "A" else int(end_key),
+                                    dir_pad_robot_count)
+         for start_key, end_key in itertools.pairwise("A" + code)]
+    ) + len(code)
 
 
 def part1(input_data: InputType) -> ResultType:
