ba812baa92be7a0bc2b356e1c3509ea8a27a33d3
Jonathan Franzi <jonoerik@fastmail.fm>
2025-05-09 19:30:14 +1000

    Complete day21 part2
    Switch back from A* to Dijkstra's Algorithm, but calculate costs for each robot in the chain
    using the previous robot's costs. Robot movement costs only depend on the robot immediately
    before them. This makes the running time scale linearly with the number of robots.

--- day21/day21.py
+++ day21/day21.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from collections.abc import Callable
 from enum import Enum
 import itertools
 from pathlib import Path
@@ -23,10 +24,6 @@ class DirKey(Enum):
     LEFT = 2
     RIGHT = 3
     A = 4
-
-# For each robot, the current key the robot is positioned to actuate. Direction pad robots are all combined into a
-# sub-tuple, to allow handling of different counts of these robots.
-SearchState = tuple[tuple[DirKey, ...], NumKey]
 
 
 def next_num_key(current_key: NumKey, dir_key: DirKey) -> NumKey | None:
@@ -71,125 +68,107 @@ def next_dir_key(current_key: DirKey, dir_key: DirKey) -> DirKey | None:
         return None
 
 
-def next_state(s: SearchState, k: DirKey) -> SearchState | None:
-    """Given a current state, and a direction key manually pressed on the first number pad, return the resulting
-    state of the system. Return None if the keypress leads to an invalid state."""
-    result_dirpad_robots = []
-    prev_key = k
-    for dirpad_robot in s[0]:
-        result_dirpad_robot = next_dir_key(dirpad_robot, prev_key)
-        if result_dirpad_robot is None:
-            return None
-        result_dirpad_robots.append(result_dirpad_robot)
-        if prev_key != DirKey.A:
-            result_dirpad_robots += s[0][len(result_dirpad_robots):]
-            return tuple(result_dirpad_robots), s[1]
-        prev_key = result_dirpad_robot
-    else:
-        assert len(result_dirpad_robots) == len(s[0])
-        result_numpad_robot = next_num_key(s[1], prev_key)
-        if result_numpad_robot is None:
-            return None
-        return tuple(result_dirpad_robots), result_numpad_robot
+# A table of the cost to move the current DirPad robot from one key to another.
+# {(from_key, to_key): number_of_manual_inputs_required}.
+DirPadTable = dict[tuple[DirKey, DirKey], int]
+# Costs for moving between keys on the NumPad.
+NumPadTable = dict[tuple[NumKey, NumKey], int]
 
 
-def calculate_heuristic(end_key: NumKey, s: SearchState) -> int:
-    """Return a heuristic estimate of the number of manual button presses to reach the required end_key on the num pad,
-    from SearchState. While this heuristic must be admissible, this A* implementation allows visited nodes to be
-    revisited if a lower cost path is found, so the heuristic isn't required to be consistent."""
-    # Gray code representation of robot states.
-    # Highest bit is 1 if the numpad robot is on end_key, 0 otherwise.
-    # Next bit is the dirpad robot immediately before the numpad robot, 0 if the robot is on the activate key,
-    # 1 otherwise.
-    # Successive dirpad robots are successive grey code bits, with the LSB being the first robot (after the manually
-    # actuated dirpad).
-    g = 1 if s[1] == end_key else 0
-    for dk in reversed(s[0]):
-        g <<= 1
-        if dk != DirKey.A:
-            g += 1
+def calculate_table[
+        KeyType
+    ](
+        next_key_function: Callable[[KeyType, DirKey], KeyType | None],
+        key_possibilities: set[KeyType],
+        prev_table: DirPadTable | None,
+        robot_index: int
+    ) -> dict[tuple[KeyType, KeyType], int]:
+    """
+    Use Dijkstra's Algorithm, and a set of movement costs from the previous directional pad robot, to calculate
+    the cost of moving this robot between any pair of two keys.
 
-    # Convert gray code representation to binary number.
-    # Wikipedia contributors. (2025, March 10). Gray code. In Wikipedia, The Free Encyclopedia.
-    # Retrieved 10:43, May 3, 2025, from https://en.wikipedia.org/w/index.php?title=Gray_code&oldid=1279704355
-    mask = g
-    while mask:
-        mask >>= 1
-        g ^= mask
+    Whether the current robot is a directional pad robot, or a number pad robot, is dictated by the KeyType generic
+    argument, and the next_key_function and key_possibilities arguments.
+    KeyType -- The type of variables representing a single robot/keypad's state.
+    next_key_function -- Function that when given the current key state, and a direction pad button pressed by the
+        previous robot in the chain, gives the resulting key for this robot (or None if the move is invalid).
+    key_possibilities -- Set of all possible values the key pad state can take.
+    prev_table -- A dictionary of the cost of moving the previous robot in the chain between any two key states.
+        If this is the first robot in the chain, i.e. if robot_index == 0, this should be None instead.
+    robot_index -- Position of this robot in the chain. 0 is the first robot after the manual keypad, 1 is the robot
+        controlled by robot 0's keypad, etc.
+    """
+    # {(from_key, to_key): number_of_manual_keypresses}
+    result: dict[tuple[KeyType, KeyType], int] = {}
+    # (previous_robot_key, current_key)
+    SearchState = tuple[DirKey, KeyType]
+    assert (robot_index == 0) == (prev_table is None)
 
-    # Take 2^bits - 1 - g, which is the bare minimum number of inputs to get to the desired end state.
-    result = 2 ** (len(s[0]) + 1) - 1 - g
+    for start_key in key_possibilities:
+        end_keys_pending = {key for key in key_possibilities}
+        # {search_state: cost_to_reach}
+        to_visit: dict[SearchState, int] = {(DirKey.A, start_key): 0}
+        # {search_state}
+        visited: set[SearchState] = set()
+
+        while end_keys_pending and to_visit:
+            previous_robot_key, current_key = min(to_visit, key=lambda k: to_visit[k])
+            current_cost = to_visit[(previous_robot_key, current_key)]
+            del to_visit[(previous_robot_key, current_key)]
+            visited.add((previous_robot_key, current_key))
+            if previous_robot_key == DirKey.A and current_key in end_keys_pending:
+                result[(start_key, current_key)] = current_cost
+                end_keys_pending.remove(current_key)
+
+            # Try actuating the previous robot's key, to move the current robot.
+            if previous_robot_key is not DirKey.A and next_key_function(current_key, previous_robot_key) is not None:
+                next_state = (previous_robot_key, next_key_function(current_key, previous_robot_key))
+                next_cost = current_cost + 1
+                if next_state not in visited and (next_state not in to_visit or to_visit[next_state] > next_cost):
+                    to_visit[next_state] = next_cost
+
+            # Try moving the previous robot to another key.
+            for target_prev_robot_key in DirKey:
+                if target_prev_robot_key == previous_robot_key:
+                    continue
+                next_state = (target_prev_robot_key, current_key)
+                if robot_index == 0:
+                    # If this is the first robot, the 'previous robot' is the manually actuated directional pad,
+                    # which can press any key without the cost of moving between them.
+                    next_cost = current_cost
+                else:
+                    next_cost = current_cost + prev_table[(previous_robot_key, target_prev_robot_key)]
+                if next_state not in visited and (next_state not in to_visit or to_visit[next_state] > next_cost):
+                    to_visit[next_state] = next_cost
+
+        assert not end_keys_pending
+
+    # Cost of transitioning between any two keys should have been calculated.
+    assert len(result) == len(key_possibilities) ** 2
     return result
 
 
-shortest_sequence_next_key_memo: dict[tuple[NumKey, NumKey, int], int] = {}
+def calculate_dir_pad_table(prev_table: DirPadTable, robot_index: int) -> DirPadTable:
+    return calculate_table(next_dir_key, set(DirKey), prev_table, robot_index)
 
 
-def shortest_sequence_next_key(start_key: NumKey, end_key: NumKey, dir_pad_robot_count: int) -> int:
-    """Use A* algorithm to find the length of the shortest sequence of manual number presses to input, to move the
-    number pad robot from key start_key to key end_key (without actuating it). All other robots start on A, and will
-    end on A as well."""
-    global shortest_sequence_next_key_memo
-    if (start_key, end_key, dir_pad_robot_count) in shortest_sequence_next_key_memo:
-        return shortest_sequence_next_key_memo[(start_key, end_key, dir_pad_robot_count)]
-
-    start_state = ((DirKey.A,) * dir_pad_robot_count, start_key)
-    # {state: (current cost, current cost + heuristic, predecessor)}
-    to_visit: dict[SearchState, tuple[int, int, SearchState | None]] = \
-        {start_state: (0, calculate_heuristic(end_key, start_state), None)}
-    # {state: (lowest cost to reach found so far, predecessor)}
-    visited: dict[SearchState, tuple[int, SearchState]] = {}
-
-    while to_visit:
-        current_state = min(to_visit, key=lambda x: to_visit[x][1])
-        cost, total_heuristic, predecessor = to_visit[current_state]
-        del to_visit[current_state]
-        if current_state == ((DirKey.A,) * dir_pad_robot_count, end_key):
-            shortest_sequence_next_key_memo[(start_key, end_key, dir_pad_robot_count)] = cost
-
-            #TODO comment
-            # Reconstruct and print the path of states.
-            path: list[SearchState] = [current_state]
-            next_path_node = predecessor
-            while next_path_node is not None:
-                path.append(next_path_node)
-                next_path_node = visited[next_path_node][1]
-            print(f"{"A" if start_key is AKey else start_key} -> {"A" if end_key is AKey else end_key}")
-            print("\t" + "\n\t".join([
-                "-".join([{DirKey.A: "A", DirKey.UP: "^", DirKey.DOWN: "v", DirKey.LEFT: "<", DirKey.RIGHT: ">"}[dirkey]
-                          for dirkey in path_node[0]]
-                         ) + "-" +
-                ("A" if path_node[1] is AKey else str(path_node[1])) for path_node in reversed(path)]))
-
-            return cost
-
-        # Allow states to be revisited, as our heuristic may not be consistent, and a better path to an earlier visited
-        # state could be found later.
-        assert current_state not in visited or visited[current_state][0] > cost
-        visited[current_state] = (cost, predecessor)
-
-        for key in DirKey:
-            next_to_visit = next_state(current_state, key)
-            if next_to_visit is None or (next_to_visit in visited and visited[next_to_visit][0] <= cost + 1):
-                continue
-            if next_to_visit not in to_visit or to_visit[next_to_visit][0] > cost + 1:
-                to_visit[next_to_visit] = (cost + 1,
-                                           cost + 1 + calculate_heuristic(end_key, next_to_visit),
-                                           current_state)
-
-    # Should have found a suitable sequence before exhausting the search space.
-    assert False
+def calculate_num_pad_table(prev_table: DirPadTable, robot_index: int) -> NumPadTable:
+    return calculate_table(next_num_key, set(range(10)) | {AKey}, prev_table, robot_index)
 
 
 def shortest_sequence(code: str, dir_pad_robot_count: int) -> int:
     """Find the length of the shortest sequence of manual button presses to input the required code into the numeric
     keypad."""
+    robot_movement_table = None
+    for i in range(dir_pad_robot_count):
+        robot_movement_table = calculate_dir_pad_table(robot_movement_table, i)
+    robot_movement_table = calculate_num_pad_table(robot_movement_table, dir_pad_robot_count)
+
     # Add up the cost of transitioning to each next numpad key, and the cost of pressing each key.
     return sum(
-        [shortest_sequence_next_key(AKey if start_key == "A" else int(start_key),
-                                    AKey if end_key == "A" else int(end_key),
-                                    dir_pad_robot_count)
-         for start_key, end_key in itertools.pairwise("A" + code)]
+        [robot_movement_table[(start_key, end_key)]
+         for start_key, end_key
+         in itertools.pairwise([AKey] + [AKey if key == "A" else int(key) for key in code])]
     ) + len(code)
 
 
@@ -198,7 +177,6 @@ def part1(input_data: InputType) -> ResultType:
     return sum([int(x[:-1]) * shortest_sequence(x, 2) for x in input_data])
 
 
-#TODO remove rounds arg
-def part2(input_data: InputType, rounds=25) -> ResultType:
+def part2(input_data: InputType) -> ResultType:
     assert all([x.endswith("A") and x[:-1].isnumeric() for x in input_data])
-    return sum([int(x[:-1]) * shortest_sequence(x, rounds) for x in input_data])
+    return sum([int(x[:-1]) * shortest_sequence(x, 25) for x in input_data])
